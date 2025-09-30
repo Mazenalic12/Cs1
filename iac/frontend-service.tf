8bcce971f58d268d6cb02ce23fd3aa686f729671
536351-Mazenalic-Feb22-Python101 <160591633+Mazenalic12@users.noreply.github.com>
2025-09-30 14:53:25 +0200

    Update frontend-service.tf

--- iac/frontend-service.tf
+++ iac/frontend-service.tf
@@ -1,29 +1,16 @@
 resource "google_cloud_run_v2_service" "frontend" {
   name     = "frontend-service"
   location = var.region
-  ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
     containers {
       image = var.image_url
-
-      ports {
-        container_port = 80
-      }
-
-      resources {
-        limits = {
-          cpu    = "1"
-          memory = "512Mi"
-        }
-      }
     }
 
     service_account = var.service_account_email
   }
 
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
+  depends_on = [
+    google_project_service.run_api
+  ]
 }
