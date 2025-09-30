8bf66228dc5aee72b9ff55d83162be35c01f5aab
536351-Mazenalic-Feb22-Python101 <160591633+Mazenalic12@users.noreply.github.com>
2025-09-30 15:37:33 +0200

    Update variables.tf

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,17 +1,23 @@
+variable "project_id" {
+  description = "The project ID where resources will be created"
+  type        = string
+  default     = "cs1-mzn-12345"
+}
+
 variable "region" {
-  description = "GCP region where resources will be created"
+  description = "Default region for resources"
   type        = string
   default     = "europe-west1"
 }
 
 variable "image_url" {
-  description = "Docker image URL in Artifact Registry"
+  description = "Docker image for frontend Cloud Run service"
   type        = string
-  default     = "europe-west1-docker.pkg.dev/cs1-mzn-12345/frontend-repo/vite-frontend"
+  default     = "gcr.io/cs1-mzn-12345/hello:latest"
 }
 
 variable "service_account_email" {
-  description = "Email of the service account used by Cloud Run"
+  description = "Service account email for Cloud Run service"
   type        = string
-  default     = "terraform-deployer@cs1-mzn-12345.iam.gserviceaccount.com"
+  default     = "cs1-mzn-12345-compute@developer.gserviceaccount.com"
 }
