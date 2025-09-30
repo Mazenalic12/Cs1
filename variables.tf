6d5c9303d7dec991bcfe2aae635c3bb483711c54
536351-Mazenalic-Feb22-Python101 <160591633+Mazenalic12@users.noreply.github.com>
2025-09-30 14:59:51 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,11 +7,13 @@ variable "region" {
 variable "image_url" {
   description = "URL of the Docker image in Artifact Registry"
   type        = string
+  # Vervang deze waarde als je het zeker weet
   default     = "europe-west1-docker.pkg.dev/cs1-mzn-12345/frontend-repo/vite-frontend"
 }
 
 variable "service_account_email" {
   description = "Email of the service account to use for Cloud Run"
   type        = string
+  # Vervang dit met jouw service account email als je wilt, of laat het leeg
   default     = "terraform-deployer@cs1-mzn-12345.iam.gserviceaccount.com"
 }
