4d86a2a931c57e48c5e7006b7833278258c08a83
536351-Mazenalic-Feb22-Python101 <160591633+Mazenalic12@users.noreply.github.com>
2025-09-30 15:35:23 +0200

    Update frontend.tf

--- iac/frontend.tf
+++ iac/frontend.tf
@@ -1,18 +1,39 @@
 resource "google_cloud_run_service" "frontend" {
   name     = "frontend-service"
-  location = "europe-west1"
-  project  = "cs1-mzn-12345"  # ✅ AANGEPAST
-
-  lifecycle {
-    prevent_destroy = false
-    ignore_changes  = all
-  }
+  location = var.region
 
   template {
     spec {
       containers {
-        image = "gcr.io/cloudrun/hello"
+        image = var.image_url
+
+        env {
+          name  = "DB_HOST"
+          value = google_sql_database_instance.postgres_instance.private_ip_address
+        }
+        env {
+          name  = "DB_PORT"
+          value = "5432"
+        }
+        env {
+          name  = "DB_NAME"
+          value = google_sql_database.default.name
+        }
+        env {
+          name  = "DB_USER"
+          value = google_sql_user.appuser.name
+        }
+        env {
+          name = "DB_PASSWORD"
+          value_from {
+            secret_key_ref {
+              name = "db-password"
+              key  = "latest"
+            }
+          }
+        }
       }
+      service_account_name = var.service_account_email
     }
   }
 
@@ -20,12 +41,4 @@ resource "google_cloud_run_service" "frontend" {
     percent         = 100
     latest_revision = true
   }
-}
-
-# Iedereen mag de Cloud Run service aanroepen (optioneel activeren)
-resource "google_cloud_run_service_iam_member" "noauth" {
-  location = google_cloud_run_service.frontend.location
-  service  = google_cloud_run_service.frontend.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
 }
