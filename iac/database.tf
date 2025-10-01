ca60b6068e209fd1e01daddd014caed4c09395d9
536351-Mazenalic-Feb22-Python101 <160591633+Mazenalic12@users.noreply.github.com>
2025-10-01 09:33:15 +0200

    Update database.tf

--- iac/database.tf
+++ iac/database.tf
@@ -1,26 +1,31 @@
 resource "google_sql_database_instance" "postgres_instance" {
   name             = "my-postgres-db"
   database_version = "POSTGRES_14"
-  region           = var.region
+  region           = "europe-west1"
 
   settings {
-    tier = "db-custom-1-3840"
+    tier = "db-f1-micro"
     ip_configuration {
       ipv4_enabled    = false
-      private_network = google_compute_network.main-vpc.self_link
+      private_network = google_compute_network.vpc_network.self_link
     }
   }
+
+  deletion_protection = false
+
+  depends_on = [
+    google_service_networking_connection.private_vpc_connection,
+    google_project_service.sqladmin
+  ]
 }
 
-# Default database
-resource "google_sql_database" "default" {
-  name     = "appdb"
+resource "google_sql_user" "default" {
+  name     = "dbuser"
   instance = google_sql_database_instance.postgres_instance.name
+  password = "Welkom01!"
 }
 
-# App user
-resource "google_sql_user" "appuser" {
-  name     = "appuser"
+resource "google_sql_database" "my_db" {
+  name     = "app_database"
   instance = google_sql_database_instance.postgres_instance.name
-  password = "ChangeMeStrongPassword!" # beter via secret manager
 }
